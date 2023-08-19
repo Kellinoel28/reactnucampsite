c0cefb93d7546dac6a2c6a3acf31bca1f44b2c8e
Kelli Kennedy <kellinoel28@gmail.com>
2023-08-18 21:53:43 -0500

    react week 3

---
+++ src/features/display/DisplayList.js
@@ -0,0 +1,26 @@
+import { Col, Row } from 'reactstrap';
+//import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
+import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
+import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import { selectFeaturedPartners } from '../partners/partnersSlice';
+
+const DisplayList = () => {
+    const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartners() ];
+
+    return (
+        <Row>
+            {items.map((item, idx) => {
+                return (
+                    item && (
+                    <Col md className='m-1' key={idx}>
+                        <AnimatedDisplayCard item={item} />
+                    </Col>
+                    )
+                );
+            })}
+        </Row>
+    );
+};
+
+export default DisplayList;
